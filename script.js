a4b2692b089a8a6622dc68ee1bba2938d1f046ee
mkruszyniak <m.kruszyniak@qarbon.it>
2022-11-30 13:08:30 +0100

    second commit

--- script.js
+++ script.js
@@ -8,19 +8,11 @@ const emailValue = document.getElementById('emailValue');
 const passwordValue = document.getElementById('passwordValue');
 const alertBox = document.querySelector('.alert');
 
-//   js code to validate email format
-function validateEmail(inputText) {
-	var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if (inputText.value.match(mailFormat)) {
-		document.loginForm.email.focus();
-		return true;
-	} else {
-		alertBox.classList.add('visible');
-		alert('You have entered an invalid email address.');
-		document.loginForm.email.focus();
-		return true;
-	}
-}
+const closeBtn = document.querySelector('.close-btn');
+const alert = closeBtn.parentElement;
+
+const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let screenWidth = window.innerWidth > 0 && window.innerWidth;
 
 //   js code to show/hide password and change icon
 showHidePassword.addEventListener('click', () => {
@@ -36,51 +28,49 @@ showHidePassword.addEventListener('click', () => {
 
 //   js code to validate if inputs don't are empty
 loginButton.addEventListener('click', () => {
-	loginButton.classList.add('inactive');
-
+	closeAlert();
+	if (!emailValue.value.match(mailFormat) && emailValue.value) {
+		renderErrorMessage('Invalid email format');
+		return;
+	}
 	if (!emailValue.value || !passwordValue.value) {
-		alert('Please, fill in all inputs.');
+		renderErrorMessage('Please, fill in all inputs.');
 	} else {
-		alert('Your password is incorrect.');
+		renderErrorMessage('Your password is incorrect.');
 	}
 });
 
-function activeSubmitButton() {
-	loginButton.classList.remove('inactive');
+// -----------------functions-----------------
+
+function renderErrorMessage(errorText) {
+	// Get the parent of <span class="close-btn"> (<div class="alert">)
+	alert.removeChild(alert.lastChild);
+
+	const error = document.createElement('div');
+	error.innerHTML = errorText;
+
+	alert.appendChild(error);
+	alert.style.display = 'block';
+	alert.style.opacity = '1';
 }
 
-function renderErrorMessage() {
-	const closeBtn = document.querySelector('.close-btn');
-	let i;
-	window.addEventListener('resize', () => {
-		var width = window.innerWidth > 0 && window.innerWidth;
-		console.log(
-			'%cMyProject%cline:55%cwidth',
-			'color:#fff;background:#ee6f57;padding:3px;border-radius:2px',
-			'color:#fff;background:#1f3c88;padding:3px;border-radius:2px',
-			'color:#fff;background:rgb(96, 143, 159);padding:3px;border-radius:2px',
-			width,
-		);
-
-		console.log(width);
-		if (width > 420) return;
-
-		// Loop through all close buttons
-		for (i = 0; i < closeBtn.length; i++) {
-			// When someone clicks on a close button
-			closeBtn[i].onclick = function () {
-				// Get the parent of <span class="closebtn"> (<div class="alert">)
-				var div = this.parentElement;
-
-				// Set the opacity of div to 0 (transparent)
-				div.style.opacity = '0';
-
-				// Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-				setTimeout(function () {
-					div.style.display = 'none';
-				}, 600);
-			};
-		}
-	});
+//   js code to validate email format
+function validateEmail(passwordInput) {
+	console.log('validation');
+	if (passwordInput.value.match(mailFormat)) {
+		document.loginForm.email.focus();
+		console.log('email zgodny');
+		closeAlert();
+	} else {
+		return renderErrorMessage('You have entered an invalid email address.');
+	}
 }
-renderErrorMessage();
+
+passwordValue.addEventListener('click', () => {
+	if (!emailValue.value.match(mailFormat) && passwordValue.value) closeAlert();
+});
+
+function closeAlert() {
+	alert.style.opacity = '0';
+	return false;
+}
