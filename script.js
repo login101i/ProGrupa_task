129de26eb38bf0580423592660a428699d2e02c4
mkruszyniak <m.kruszyniak@qarbon.it>
2022-12-01 01:24:33 +0100

    styling udpdate

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-const container = document.querySelector('.container'),
-	showHidePassword = document.querySelector('.showHidePassword'),
-	passwordField = document.querySelector('.password'),
-	signUp = document.querySelector('.signup-link'),
-	loginButton = document.querySelector('.submit-button');
-
-const emailValue = document.getElementById('emailValue');
-const passwordValue = document.getElementById('passwordValue');
+const container = document.querySelector('.container');
+const eyeIcon = document.querySelector('.eye-icon');
+const passwordField = document.querySelector('.password');
+const loginButton = document.querySelector('.submit-button');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
 const alertBox = document.querySelector('.alert');
 const loadAnimation = document.querySelector('.loader');
-
+const eyeIconContainer = document.querySelector('.eye-icon-container');
 const closeBtn = document.querySelector('.close-btn');
 const alert = closeBtn.parentElement;
-const loginButton2 = document.getElementById('loginButton');
 
 const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-let screenWidth = window.innerWidth > 0 && window.innerWidth;
+
 
 //   js code to show/hide password and change icon
-showHidePassword.addEventListener('click', () => {
-	if (!passwordValue.value) return;
+eyeIconContainer.addEventListener('click', () => {
 	if (passwordField.type === 'password') {
 		passwordField.type = 'text';
-		showHidePassword.classList.replace('uil-eye-slash', 'uil-eye');
+		eyeIcon.classList.replace('uil-eye-slash', 'uil-eye');
 	} else {
 		passwordField.type = 'password';
-		showHidePassword.classList.replace('uil-eye', 'uil-eye-slash');
+		eyeIcon.classList.replace('uil-eye', 'uil-eye-slash');
 	}
 });
 
 //   js code to validate if inputs don't are empty
 loginButton.addEventListener('click', () => {
 	closeAlert();
-	if (!emailValue.value.match(mailFormat) && emailValue.value) {
+	if (!emailInput.value.match(mailFormat) && emailInput.value) {
 		renderErrorMessage('Invalid email format');
 		return;
 	}
-	if (!emailValue.value || !passwordValue.value) {
+	if (!emailInput.value || !passwordInput.value) {
 		renderErrorMessage('Please, fill in all inputs');
 	} else {
 		showLoader();
 		setTimeout(() => {
 			renderErrorMessage('Your password is incorrect');
-			// focus again on password input
-			document.loginForm.password.focus();
 		}, 2000);
 	}
 });
+
+passwordInput.addEventListener('click', () => {
+	if (!emailInput.value.match(mailFormat) && passwordInput.value) closeAlert();
+});
+
 
 // -----------------functions-----------------
 
@@ -71,18 +70,13 @@ function renderErrorMessage(errorText) {
 }
 
 //   js code to validate email format
-function validateEmail(passwordInput) {
-	if (passwordInput.value.match(mailFormat)) {
-		document.loginForm.email.focus();
+function validateEmail(emailInput) {
+	if (emailInput.value.match(mailFormat)) {
 		closeAlert();
 	} else {
 		return renderErrorMessage('Invalid e-mail address');
 	}
 }
-
-passwordValue.addEventListener('click', () => {
-	if (!emailValue.value.match(mailFormat) && passwordValue.value) closeAlert();
-});
 
 function closeAlert() {
 	alert.style.display = 'none';
--- utils/validators.js
+++
@@ -1,9 +0,0 @@
-const requiredEmail = value => {
-	if (!value) return 'wymagane';
-	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	if (value.match(validRegex)) {
-		return false;
-	} else {
-		return 'Wymagane';
-	}
-};
